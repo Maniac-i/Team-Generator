c1ff04fbb8c7d36c120b287bd14e7522a405ae2a
Joe Maniaci <joe.maniaci1@gmail.com>
2021-01-05 18:26:05 -0500

    added catch to end of inquire prompt to handle errors

--- app.js
+++ app.js
@@ -16,119 +16,121 @@ const render = require("./lib/htmlRenderer");
 
 let output = [];
 
-   let questions = [
-    {
-      type: 'confirm',
-      name: 'manager',
-      message: 'Would you like to add a Manager?',
-      default: true,
-      when: () => !output.some(output => output instanceof Manager),
-    },
-    {
-      type: 'list',
-      name: 'role',
-      message: 'What is the role of this team member?',
-      choices: ['Manager'],
-      when: function (answers) {
-        return answers.manager == true;
-      }
-    },
-    {
-      type: 'list',
-      name: 'role',
-      message: 'What role would you like to add?',
-      choices: ['Engineer', 'Intern'],
-      when: function (answers) {
-        return answers.manager == false || !answers.manager;
-      }
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'What is the team memebers office number?',
-      when: function (answers) {
-        return answers.role == 'Manager';
-      }
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'What is the team memebers GitHub profile?',
-      when: function (answers) {
-        return answers.role == 'Engineer';
-      }
-    },
-    {
-      type: 'input',
-      name: 'school',
-      message: 'What school did the team member go to?',
-      when: function (answers) {
-        return answers.role == 'Intern';
-      }
-    },
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the team memebers name?',
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: 'What is the team memebers ID?',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is the team memebers email?',
-    },
-    {
-      type: 'confirm',
-      name: 'addAnother',
-      message: 'Would you like to add another employee?',
-      default: true,
-    },
-  ];
-  
-  function ask() {
-    return inquirer.prompt(questions).then((answers) => {
-      
-      switch (answers.role) {
-      case "Manager": 
+let questions = [
+  {
+    type: 'confirm',
+    name: 'manager',
+    message: 'Would you like to add a Manager?',
+    default: true,
+    when: () => !output.some(output => output instanceof Manager),
+  },
+  {
+    type: 'list',
+    name: 'role',
+    message: 'What is the role of this team member?',
+    choices: ['Manager'],
+    when: function (answers) {
+      return answers.manager == true;
+    }
+  },
+  {
+    type: 'list',
+    name: 'role',
+    message: 'What role would you like to add?',
+    choices: ['Engineer', 'Intern'],
+    when: function (answers) {
+      return answers.manager == false || !answers.manager;
+    }
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is the team memebers office number?',
+    when: function (answers) {
+      return answers.role == 'Manager';
+    }
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is the team memebers GitHub profile?',
+    when: function (answers) {
+      return answers.role == 'Engineer';
+    }
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: 'What school did the team member go to?',
+    when: function (answers) {
+      return answers.role == 'Intern';
+    }
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the team memebers name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is the team memebers ID?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the team memebers email?',
+  },
+  {
+    type: 'confirm',
+    name: 'addAnother',
+    message: 'Would you like to add another employee?',
+    default: true,
+  },
+];
+
+function ask() {
+  return inquirer.prompt(questions).then((answers) => {
+
+    switch (answers.role) {
+      case "Manager":
         output.push(new Manager(answers.name, answers.id, answers.email, answers.officeNumber));
-       console.log(output);
+        console.log(output);
         break;
-        
-      case "Engineer": 
+
+      case "Engineer":
         output.push(new Engineer(answers.name, answers.id, answers.email, answers.github));
+        console.log(output);
         break;
-        
-      case "Intern": 
-          output.push(new Intern(answers.name, answers.id, answers.email, answers.school));
-          break;
-        
+
+      case "Intern":
+        output.push(new Intern(answers.name, answers.id, answers.email, answers.school));
+        console.log(output);
+        break;
+
       default:
         console.log("The role was not recognized");
-      }
+    }
 
-      if (answers.addAnother === true) {
-        
-        return ask();
+    if (answers.addAnother === true) {
 
-      } else { 
+      return ask();
 
-        const rendered = render(output);
+    } else {
 
-        fs.writeFile(outputPath, rendered, (err) => {
-          if (err) return console.log(err);
-          console.log("File successfully created!");
-        })
-      }
-    })
-  };
+      const rendered = render(output);
 
-  ask()
+      fs.writeFile(outputPath, rendered, (err) => {
+        if (err) return console.log(err);
+        console.log("File successfully created!");
+      })
+    }
+  }).catch()   
+};
 
-  
+ask()
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
